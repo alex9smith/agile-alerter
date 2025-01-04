b81742f5904ea37485748026503c68a7c6916729
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-01-04 20:11:23 +0000

    Update to latest Agile tariff code

--- app/constants.py
+++ app/constants.py
@@ -1,1 +1,1 @@
-PRICING_BASE_URL = "https://api.octopus.energy/v1/products/AGILE-FLEX-22-11-25/electricity-tariffs/E-1R-AGILE-FLEX-22-11-25-REGION/standard-unit-rates/"
+PRICING_BASE_URL = "https://api.octopus.energy/v1/products/AGILE-24-10-01/electricity-tariffs/E-1R-AGILE-24-10-01-C/standard-unit-rates/"
